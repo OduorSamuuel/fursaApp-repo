ba3ce02a39a33d1668276e88346316eec6b25ed5
OduorSamuue <onyangos949@gmail.com>
2024-07-15 03:04:04 +0300

    changes on images and frontend

--- resources/js/Layouts/Chat.jsx
+++ resources/js/Layouts/Chat.jsx
@@ -6,9 +6,13 @@ import { router, usePage } from "@inertiajs/react";
 import { debounce } from "lodash";
 import Layout from './Layout';
 import Accounts from './Accounts/Accounts';
+import SidebarLayout from '../Layouts/Admin/SidebarLayout'; // Ensure this import matches your actual file structure
+import TopBar from './Admin/TopBar';
+import Sidebar from './Admin/Sidebar';
 
 export default function App({ children }) {
     const { auth } = usePage().props;
+    console.log("Auth ansnsn", auth.adminTitle);
 
     useEffect(() => {
         const debouncedReload = debounce(() => {
@@ -37,7 +41,7 @@ export default function App({ children }) {
 
     const renderSidebarScreen = () => {
         const currentPath = route().current();
-        let className = "px-5 py-2 pb-5 lg:w-1/3 lg:border-r lg:border-gray-300   bg-violet-300 ";
+        let className = "px-5 py-2 pb-5 lg:w-1/3 lg:border-r lg:border-gray-300 bg-violet-300";
 
         if (currentPath === 'chat.index') className += "flex flex-col w-full";
         else className += "hidden flex-col lg:flex";
@@ -45,19 +49,55 @@ export default function App({ children }) {
         return className;
     };
 
+    if (auth.user.is_admin) {
+        if (auth.adminTitle === 'general_admin') {
+            return (
+                <SidebarLayout user={auth.user}>
+                    <main className="nxl-container">
+                        <div className="nxl-content">
+                            <div className="px-6 mx-auto max-w-screen-2xl xl:px-0">
+                                <div className="flex overflow-hidden w-full h-96 shadow">
+                                    <div className={renderSidebarScreen()}>
+                                        <MineProfileChat auth={auth} />
+                                        <ChatListUser />
+                                    </div>
+                                    {children}
+                                </div>
+                            </div>
+                        </div>
+                    </main>
+                </SidebarLayout>
+            );
+        } else if (auth.adminTitle === 'serviceprovider_admin') {
+            return (
+                <TopBar>
+                    <Sidebar>
+                        <div className="px-6 mx-auto max-w-screen-2xl xl:px-0">
+                            <div className="flex overflow-hidden w-full h-96 shadow">
+                                <div className={renderSidebarScreen()}>
+                                    <MineProfileChat auth={auth} />
+                                    <ChatListUser />
+                                </div>
+                                {children}
+                            </div>
+                        </div>
+                    </Sidebar>
+                </TopBar>
+            );
+        }
+    }
+
     return (
         <Layout auth={auth}>
             <Accounts>
-                <div className="px-6 mx-auto max-w-screen-2xl xl:px-0  ">
-                   
-                        <div className="flex  overflow-hidden  w-full h-96 shadow">
-                            <div className={renderSidebarScreen()}>
-                                <MineProfileChat auth={auth} />
-                                <ChatListUser />
-                            </div>
-                            {children}
+                <div className="px-6 mx-auto max-w-screen-2xl xl:px-0">
+                    <div className="flex overflow-hidden w-full h-96 shadow">
+                        <div className={renderSidebarScreen()}>
+                            <MineProfileChat auth={auth} />
+                            <ChatListUser />
                         </div>
-                    
+                        {children}
+                    </div>
                 </div>
             </Accounts>
         </Layout>
